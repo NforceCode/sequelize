8add506861cd03d8deb0655d28fd5f41126625f5
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-04-02 19:18:45 +0300

    Fix Model naming

--- models/book.js
+++ models/book.js
@@ -1,7 +1,7 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class book extends Model {
+  class Book extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  book.init(
+  Book.init(
     {
       name: {
         type: DataTypes.STRING,
@@ -57,10 +57,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'book',
+      modelName: 'Book',
       tableName: 'books',
       underscored: true
     }
   );
-  return book;
+  return Book;
 };
